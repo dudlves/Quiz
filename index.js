9f8349b7ea57cb7175f1886b51623e6b5599e683
dudlves <dudlves17@gmail.com>
2024-02-08 21:35:33 -0300

    feat: create Js, add questions

---
+++ index.js
@@ -0,0 +1,92 @@
+const perguntas = [
+  {
+    pergunta: "O que é JavaScript?",
+    respostas: [
+      "Uma linguagem de marcação",
+      "Uma linguagem de programação",
+      "Um sistema operacional"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual é a função do operador 'typeof' em JavaScript?",
+    respostas: [
+      "Converter tipos de dados",
+      "Verificar o tipo de uma variável",
+      "Realizar operações matemáticas"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "O que é uma variável em JavaScript?",
+    respostas: [
+      "Uma função",
+      "Um valor constante",
+      "Um contêiner para armazenar dados"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual é a sintaxe correta para comentários de uma linha em JavaScript?",
+    respostas: [
+      "// Este é um comentário",
+      "# Este é um comentário",
+      "/* Este é um comentário */"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "O que é o DOM em JavaScript?",
+    respostas: [
+      "Um modelo de dados",
+      "Um tipo de variável",
+      "A representação da estrutura da página web"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Como você declara uma função em JavaScript?",
+    respostas: [
+      "função minhaFuncao()",
+      "def minhaFuncao()",
+      "function minhaFuncao()"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "O que é um loop 'for' em JavaScript?",
+    respostas: [
+      "Um dispositivo de entrada",
+      "Um comando de saída",
+      "Uma estrutura de controle de fluxo"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual é a diferença entre '==' e '===' em JavaScript?",
+    respostas: [
+      "Não há diferença",
+      "Um compara apenas o valor, o outro compara valor e tipo",
+      "Um compara apenas o tipo, o outro compara valor e tipo"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "O que é AJAX em JavaScript?",
+    respostas: [
+      "Uma linguagem de programação",
+      "Uma técnica para atualização assíncrona de dados na página",
+      "Um framework para construção de interfaces gráficas"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "O que é JSON em JavaScript?",
+    respostas: [
+      "Um formato de arquivo de áudio",
+      "Uma linguagem de marcação",
+      "Um formato de dados para intercâmbio"
+    ],
+    correta: 2
+  }
+];
